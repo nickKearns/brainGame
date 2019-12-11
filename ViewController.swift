c250f024c4ec174977eea71c91371c89150e552c
nickKearns <nickkearns133@gmail.com>
2019-12-10 23:21:20 -0800

    init

---
+++ ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Color Game
+//
+//  Created by Nicholas Kearns on 12/10/19.
+//  Copyright © 2019 Nicholas Kearns. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var yesButton: UIButton!
+    @IBOutlet weak var noButton: UIButton!
+    
+
+    
+    var score = 0
+    
+    @IBAction func yesButtonTapped(_ sender: Any) {
+        
+        //if yes is tapped and the correct answer is yes
+        //increment score by 1
+        
+    }
+    
+    @IBAction func noButtonTapped(_ sender: Any) {
+        
+        //if no is tapped and the correct answer is no
+        //increment score by 1
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+    
+
+}
+
